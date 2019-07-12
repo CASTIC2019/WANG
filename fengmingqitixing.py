eb771d3121f439c612671261edf8e6e88a406999
YBillWangziyin <13585877001@163.com>
2019-07-12 15:24:48 +0800

    sfdfaskdfl

--- fengmingqitixing.py
+++ fengmingqitixing.py
@@ -20,10 +20,10 @@ for p in ports:
         print ("No Arduino Device was found connected to the computer")
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-#eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
-cap = cv2.VideoCapture(0)
+imgface = cv2.imread('face.jpg')
+cap = cv2.VideoCapture(1)
 while(True):
+    cv2.imwrite('face.jpg',imgface[0:10,0:10])
     ret,img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)    
     faces = face_cascade.detectMultiScale(gray,1.3,5)
@@ -32,10 +32,7 @@ while(True):
         cv2.imwrite('face.jpg',img[y:y+h,x:x+w])
         roi_gray = gray[y:y+h,x:x+w]
         roi_color = img[y:y+h,x:x+w]
-        #eyes = eye_cascade.detectMultiScale(roi_gray)
-        #for (ex,ey,ew,eh) in eyes:
-            #cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-    #cv2.imshow('img',img)
+    cv2.imshow('img',img)
     imgface = cv2.imread('face.jpg')
     (w,h,ctrl)=imgface.shape
     if w*h>40000:
@@ -47,8 +44,7 @@ while(True):
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
-#cv2.destroyAllWindows()
-
+cv2.destroyAllWindows()
 
 # In[ ]:
 
